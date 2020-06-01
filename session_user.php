9f6aafa597ce5c4871c44c435495283474c6f7cc
egooktafanda97 <48469529+egooktafanda97@users.noreply.github.com>
2020-06-02 03:05:39 +0700

    Update session_user.php

--- session_user.php
+++ session_user.php
@@ -1,1 +1,6 @@
 
+<?php
+	if (!isset($_SESSION['user']) OR !isset($_COOKIE['username'])) {
+		$_SESSION['hasil'] = array('alert' => 'danger', 'pesan' => 'Silahkan Masuk Terlebih Dahulu.');
+		exit(header("Location: ".$config['web']['url']."auth/login"));
+	}
